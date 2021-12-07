2667fe68e50d8d8d39ec0c286565753756710381
Marcelo Cordova <cordovam@g.cofc.edu>
2021-12-07 15:42:18 -0500

    Updated trigger script

--- src/main/resources/db/migration/V5__db_triggers.sql
+++ src/main/resources/db/migration/V5__db_triggers.sql
@@ -1,4 +1,9 @@
-CREATE FUNCTION add_passing() RETURNS trigger AS $add_passing$
+DROP TRIGGER add_passing on passing;
+DROP TRIGGER add_receiving on receiving;
+DROP TRIGGER add_rushing on rushing;
+DROP TRIGGER add_defense on defense;
+
+CREATE OR REPLACE FUNCTION add_passing() RETURNS trigger AS $add_passing$
     BEGIN
         -- Check that f_name, l_name, and pos are given
         IF NEW.f_name is NULL THEN
@@ -18,10 +23,10 @@ CREATE FUNCTION add_passing() RETURNS trigger AS $add_passing$
     END;
 $add_passing$ LANGUAGE plpgsql;
 
-CREATE TRIGGER add_passing BEFORE INSERT OR UPDATE on passing
+CREATE TRIGGER add_passing AFTER INSERT ON passing
     FOR EACH ROW EXECUTE PROCEDURE add_passing();
 
-CREATE FUNCTION add_rushing() RETURNS trigger AS $add_rushing$
+CREATE OR REPLACE FUNCTION add_rushing() RETURNS trigger AS $add_rushing$
     BEGIN
         -- Check that f_name, l_name, and pos are given
         IF NEW.f_name is NULL THEN
@@ -41,10 +46,10 @@ CREATE FUNCTION add_rushing() RETURNS trigger AS $add_rushing$
     END;
 $add_rushing$ LANGUAGE plpgsql;
 
-CREATE TRIGGER add_rushing BEFORE INSERT OR UPDATE on rushing
+CREATE TRIGGER add_rushing AFTER INSERT ON rushing
     FOR EACH ROW EXECUTE PROCEDURE add_rushing();
 
-CREATE FUNCTION add_receiving() RETURNS trigger AS $add_receiving$
+CREATE OR REPLACE FUNCTION add_receiving() RETURNS trigger AS $add_receiving$
     BEGIN
         -- Check that f_name, l_name, and pos are given
         IF NEW.f_name is NULL THEN
@@ -64,10 +69,10 @@ CREATE FUNCTION add_receiving() RETURNS trigger AS $add_receiving$
     END;
 $add_receiving$ LANGUAGE plpgsql;
 
-CREATE TRIGGER add_receiving BEFORE INSERT OR UPDATE on receiving
+CREATE TRIGGER add_receiving AFTER INSERT ON receiving
     FOR EACH ROW EXECUTE PROCEDURE add_receiving();
 
-CREATE FUNCTION add_defense() RETURNS trigger AS $add_defense$
+CREATE OR REPLACE FUNCTION add_defense() RETURNS trigger AS $add_defense$
     BEGIN
         -- Check that f_name, l_name, and pos are given
         IF NEW.f_name is NULL THEN
@@ -87,5 +92,5 @@ CREATE FUNCTION add_defense() RETURNS trigger AS $add_defense$
     END;
     $add_defense$ LANGUAGE plpgsql;
 
-CREATE TRIGGER add_defense BEFORE INSERT OR UPDATE on defense
+CREATE TRIGGER add_defense AFTER INSERT ON defense
     FOR EACH ROW EXECUTE PROCEDURE add_defense();
